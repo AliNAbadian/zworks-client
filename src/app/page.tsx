9d46c3e5d938d9ab289fbc6299cf0e16d938d685
AliNAbadian <nagshrizali@gmail.com>
2025-09-17 22:38:36 +0330

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button
+        // variant={"secondary"}
+        className="cursor-pointer font-iransans "
+      >
+        سلام
+      </Button>
+    </div>
+  );
+};
+
+export default page;
